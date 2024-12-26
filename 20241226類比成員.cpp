95df7f72c49422c5659ef88ee3a892f81cbcfe6f
15sssssssssss <313015@s.lhvs.tyc.edu.tw>
2024-12-26 13:56:46 +0800

    Update 20241226類比成員.cpp

--- 20241226類比成員.cpp
+++ 20241226類比成員.cpp
@@ -1,1 +1,32 @@
 # c
+#include<iostream>
+using namespace std;
+
+class myTime{
+	private:
+		int hour;
+		int minute;
+		bool validTime(int h, int m);
+    public:
+		bool setTime(int h, int m);
+		int getHour(){freturn hour;}
+		int getMinute(){return minute;}
+		void printTime();
+};
+
+bool myTime::validTime(int h, int m){
+	if(h < 0 || h > 23) return false;
+	if(m < 0 || m > 59) return false;
+	return true;
+}
+bool myTime::setTime(int h, int m){
+	if(validTime(h,m)){
+	    hour = h;
+	    minute = m;
+	    return ture;
+	}
+	else return false;	
+}
+void myTime::printTime(){
+	cont<<hour<<":"<<minute<<"\n";
+}   
